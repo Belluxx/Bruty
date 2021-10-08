8d2d3299aa3d9a943a0b9138be3f5a79645b3f16
Pietro Bellodi <78388441+Belluxx@users.noreply.github.com>
2021-10-08 18:55:28 +0200

    Improve usage message

--- src/main.cpp
+++ src/main.cpp
@@ -6,24 +6,24 @@ using namespace std;
 #define NOT_FOUND (-1)
 #define bigint unsigned long long int
 
-static const int BAREBONE_DICT_SIZE = 26;
-static const char BAREBONE_DICT[BAREBONE_DICT_SIZE] =   {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+static const int BAREBONE_CHARSET_SIZE = 26;
+static const char BAREBONE_CHARSET[BAREBONE_CHARSET_SIZE] =   {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-static const int MINIMAL_DICT_SIZE = 36;
-static const char MINIMAL_DICT[MINIMAL_DICT_SIZE] =     {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+static const int MINIMAL_CHARSET_SIZE = 36;
+static const char MINIMAL_CHARSET[MINIMAL_CHARSET_SIZE] =     {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                                                          '0','1','2','3','4','5','6','7','8','9'};
 
-static const int BASIC_DICT_SIZE = 52;
-static const char BASIC_DICT[BASIC_DICT_SIZE] =         {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+static const int BASIC_CHARSET_SIZE = 52;
+static const char BASIC_CHARSET[BASIC_CHARSET_SIZE] =         {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                                                          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
-static const int NORMAL_DICT_SIZE = 62;
-static const char NORMAL_DICT[NORMAL_DICT_SIZE] =       {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+static const int NORMAL_CHARSET_SIZE = 62;
+static const char NORMAL_CHARSET[NORMAL_CHARSET_SIZE] =       {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                                                          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
                                                          '0','1','2','3','4','5','6','7','8','9'};
 
-static const int BIG_DICT_SIZE = 70;
-static const char BIG_DICT[BIG_DICT_SIZE] =             {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
+static const int BIG_CHARSET_SIZE = 70;
+static const char BIG_CHARSET[BIG_CHARSET_SIZE] =             {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
                                                          'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',
                                                          '0','1','2','3','4','5','6','7','8','9',
                                                          '!','#','-','_','$','%','&','*'};
@@ -40,7 +40,7 @@ void next_direct(char*, const char*);
 int ctoi(char, const char*);
 bool is_last(char, const char*);
 
-int dict_size = -1;
+int charset_size = -1;
 int pass_size = -1;
 char* pass;
 
@@ -70,33 +70,33 @@ int main(int argc, char const *argv[]) {
 
     switch (mode) {
         case 0:
-            dict_size = BAREBONE_DICT_SIZE;
-            if (fwrite_enabled) bruteforce_singlethread(BAREBONE_DICT, fname);
-            else bruteforce_singlethread(BAREBONE_DICT);
+            charset_size = BAREBONE_CHARSET_SIZE;
+            if (fwrite_enabled) bruteforce_singlethread(BAREBONE_CHARSET, fname);
+            else bruteforce_singlethread(BAREBONE_CHARSET);
             break;
 
         case 1:
-            dict_size = MINIMAL_DICT_SIZE;
-            if (fwrite_enabled) bruteforce_singlethread(MINIMAL_DICT, fname);
-            else bruteforce_singlethread(MINIMAL_DICT);
+            charset_size = MINIMAL_CHARSET_SIZE;
+            if (fwrite_enabled) bruteforce_singlethread(MINIMAL_CHARSET, fname);
+            else bruteforce_singlethread(MINIMAL_CHARSET);
             break;
 
         case 2:
-            dict_size = BASIC_DICT_SIZE;
-            if (fwrite_enabled) bruteforce_singlethread(BASIC_DICT, fname);
-            else bruteforce_singlethread(BASIC_DICT);
+            charset_size = BASIC_CHARSET_SIZE;
+            if (fwrite_enabled) bruteforce_singlethread(BASIC_CHARSET, fname);
+            else bruteforce_singlethread(BASIC_CHARSET);
             break;
 
         case 3:
-            dict_size = NORMAL_DICT_SIZE;
-            if (fwrite_enabled) bruteforce_singlethread(NORMAL_DICT, fname);
-            else bruteforce_singlethread(NORMAL_DICT);
+            charset_size = NORMAL_CHARSET_SIZE;
+            if (fwrite_enabled) bruteforce_singlethread(NORMAL_CHARSET, fname);
+            else bruteforce_singlethread(NORMAL_CHARSET);
             break;
 
         case 4:
-            dict_size = BIG_DICT_SIZE;
-            if (fwrite_enabled) bruteforce_singlethread(BIG_DICT, fname);
-            else bruteforce_singlethread(BIG_DICT);
+            charset_size = BIG_CHARSET_SIZE;
+            if (fwrite_enabled) bruteforce_singlethread(BIG_CHARSET, fname);
+            else bruteforce_singlethread(BIG_CHARSET);
             break;
 
         default:
@@ -105,17 +105,17 @@ int main(int argc, char const *argv[]) {
     }
 }
 
-void bruteforce_singlethread(const char* _dict, const string& fname) {
-    fill_array(pass, pass_size, _dict[0]);
+void bruteforce_singlethread(const char* _charset, const string& fname) {
+    fill_array(pass, pass_size, _charset[0]);
     ofstream f;
     f.open(fname);
 
-    const bigint field_size = pow(dict_size, pass_size) - 1;
+    const bigint field_size = pow(charset_size, pass_size) - 1;
     const bigint progress_step = field_size / 1000;
     cout << "Attempts needed: " << field_size + 1 << endl;
     f << pass << endl;
     for (bigint i = 0; i < field_size; i++) {
-        next_direct(pass, _dict);
+        next_direct(pass, _charset);
         f << pass << endl;
         if (i % progress_step == 0) {
             float progress = ((float) i / (float) field_size) * 100.0f;
@@ -126,15 +126,15 @@ void bruteforce_singlethread(const char* _dict, const string& fname) {
     cout << endl;
 }
 
-void bruteforce_singlethread(const char* _dict) {
-    fill_array(pass, pass_size, _dict[0]);
+void bruteforce_singlethread(const char* _charset) {
+    fill_array(pass, pass_size, _charset[0]);
 
-    const bigint field_size = pow(dict_size, pass_size) - 1;
+    const bigint field_size = pow(charset_size, pass_size) - 1;
     const bigint progress_step = field_size / 1000;
     clog << "Attempts needed: " << field_size << endl;
     cout << pass << endl;
     for (bigint i = 0; i < field_size; i++) {
-        next_direct(pass, _dict);
+        next_direct(pass, _charset);
         cout << pass << endl;
         if (i % progress_step == 0) {
             float progress = ((float) i / (float) field_size) * 100.0f;
@@ -145,37 +145,37 @@ void bruteforce_singlethread(const char* _dict) {
     clog << endl;
 }
 
-void next_direct(char* _pass, const char* _dict) {
+void next_direct(char* _pass, const char* _charset) {
     const char c = _pass[0];
-    if (is_last(c, _dict)) {
+    if (is_last(c, _charset)) {
         for (int i = 0; i < pass_size; i++) {
-            if (_pass[i] == _dict[dict_size - 1]) {
-                _pass[i] = _dict[0];
+            if (_pass[i] == _charset[charset_size - 1]) {
+                _pass[i] = _charset[0];
             } else {
-                _pass[i] = next_char(_pass[i], _dict);
+                _pass[i] = next_char(_pass[i], _charset);
                 break;
             }
         }
     } else {
-        _pass[0] = next_char(c, _dict);
+        _pass[0] = next_char(c, _charset);
     }
 }
 
-char next_char(char c, const char* _dict) {
-    const int index = ctoi(c, _dict);
-    if (index != dict_size - 1)
-        return _dict[index + 1];
+char next_char(char c, const char* _charset) {
+    const int index = ctoi(c, _charset);
+    if (index != charset_size - 1)
+        return _charset[index + 1];
     else
-        return _dict[0];
+        return _charset[0];
 }
 
-bool is_last(char c, const char* _dict) {
-    return c == _dict[dict_size - 1];
+bool is_last(char c, const char* _charset) {
+    return c == _charset[charset_size - 1];
 }
 
-int ctoi(char c, const char* _dict) {
-    for (int i = 0; i < dict_size; i++) {
-        if (_dict[i] == c) return i;
+int ctoi(char c, const char* _charset) {
+    for (int i = 0; i < charset_size; i++) {
+        if (_charset[i] == c) return i;
     }
     return NOT_FOUND;
 }
@@ -190,13 +190,13 @@ void show_info() {
 }
 
 void show_help() {
-    clog << "Usage: bruty dictionary length [-f out_file]" << endl;
-    clog << "\t- dictionary:\n"
-            "\t\t0: Barebone  abc..xyz\n"
-            "\t\t1: Minimal   abc..012\n"
-            "\t\t2: Basic     abc..XYZ\n"
-            "\t\t3: Normal    abc..012..XYZ\n"
-            "\t\t4: Big       abc..012..XYZ..%&*\n";
+    clog << "Usage: bruty charset length [-f out_file]" << endl;
+    clog << "\t- charset: the set of characters that composes the passwords\n"
+            "\t\t0: Barebone ->   lowercase letters\n"
+            "\t\t1: Minimal  ->   lowercase letters and digits\n"
+            "\t\t2: Basic    ->   lowercase and uppercase letters\n"
+            "\t\t3: Normal   ->   lowercase and uppercase letters and digits\n"
+            "\t\t4: Big      ->   lowercase and uppercase letters, digits and symbols\n";
     clog << "\t- length: the length of the password\n";
     clog << "\t- out_file: the name of the file where passwords will be written\n";
     exit(1);
